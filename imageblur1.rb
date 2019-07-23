cb27dd8028c5ff87bbed13b5a9c9ffd56d68fa13
rayazanderson <rayazand@gmail.com>
2019-07-23 03:23:14 +0000

    image blur 1-2

--- imageblur1.rb
+++ imageblur1.rb
@@ -1,4 +1,5 @@
-class Image
+class Image 
+    attr_accessor :array
 
   def initialize(array)
     @image = array
@@ -10,39 +11,11 @@ class Image
     end
   end
 
-  def blur!(distance=1)
-    distance.times do
-      blur_coords!
-    end
-  end
-
-  private
-
-    def blur_coords!
-      blur_coords = []
-      @image.each_with_index do |row, i|
-        row.each_with_index do |x, row_i|
-          blur_coords << [i, row_i] if x == 1
-        end
-      end
-
-      blur_coords.each do |coord|
-        @image[coord[0]][coord[1] + 1] = 1 if coord[1] + 1 <= @image[coord[0]].length - 1
-        @image[coord[0]][coord[1] - 1] = 1 if coord[1] - 1 >= 0
-        @image[coord[0] + 1][coord[1]] = 1 if coord[0] + 1 <= @image.length - 1
-        @image[coord[0] - 1][coord[1]] = 1 if coord[0] - 1 >= 0
-      end
-    end
-
-end
-
 image = Image.new([
-  [0, 0, 0, 0, 0, 0],
-  [0, 1, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0]
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
 ])
-image.blur!(2)
+image.blur! 
 image.output_image
